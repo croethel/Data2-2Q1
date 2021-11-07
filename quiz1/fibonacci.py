aa96e273442d7f367878b7041c017dc36d14e46a
Christine Roethel <roethel.christine@gmail.com>
2021-11-07 15:50:04 -0500

    exercise 2 complete

--- quiz1/fibonacci.py
+++ quiz1/fibonacci.py
@@ -2,4 +2,12 @@ def fibonacci(fin: float) -> float:
     """
     build a classic fibonacci function - No RECURSION
     """
-    pass  # implement me
+    a = 0
+    b = 1
+
+    if fin >= 0:
+        for i in range(1, fin, 1):
+            c = a + b
+            a = b
+            b = c
+        return c
