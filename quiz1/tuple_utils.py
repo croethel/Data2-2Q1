efb86ebe6da817383cebe34e19e826ec7dda5879
Christine Roethel <roethel.christine@gmail.com>
2021-11-07 16:16:10 -0500

    completed exercise 4

--- quiz1/tuple_utils.py
+++ quiz1/tuple_utils.py
@@ -13,7 +13,7 @@ def tic_tac_toe_finish(board: TicTacToeBoard, pos_y: int, pos_x: int, symbol: st
     :param symbol: The symbol that should be placed in the column (X, or O)
     :return: None
     """
-    pass  # remove pass statement and implement me
+    board[pos_y][pos_x] = symbol
 
 
 def count_things(collection: Tuple, thing: Union[int, str]) -> int:
@@ -23,11 +23,9 @@ def count_things(collection: Tuple, thing: Union[int, str]) -> int:
     :param thing: An item in the collection parameter
     :return: An integer.
     """
-    pass  # remove pass statement and implement me
+    return collection.count(thing)
 
 
 def produce_tuple(alist: List) -> Tuple:
-    """
-    Produce a specific tuple.
-    """
-    pass # remove pass statement and implement me
+    alist = ['Pride', 'Envy', 'Gluttony', 'Lust', 'Anger', 'Greed', 'Sloth']
+    return tuple(alist)
